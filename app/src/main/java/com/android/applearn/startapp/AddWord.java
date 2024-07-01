e8483f9749738625cf2af93c6d41266a122ca561
Novdmyt <135864604+Novdmyt@users.noreply.github.com>
2024-07-01 15:15:00 +0300

    Додано можливість додовати слова до бази даних.

--- app/src/main/java/com/android/applearn/startapp/AddWord.java
+++ app/src/main/java/com/android/applearn/startapp/AddWord.java
@@ -3,12 +3,34 @@ package com.android.applearn.startapp;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.android.applearn.R;
+import com.android.applearn.addword.AddWordOnDataBase;
 
 public class AddWord extends AppCompatActivity {
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.create_card);
+        setContentView(R.layout.add_word);
+
+        // Check if the savedInstanceState is null to avoid overlapping fragments
+        if (savedInstanceState == null) {
+            // Create an instance of the fragment
+            AddWordOnDataBase addWordOnDataBaseFragment = new AddWordOnDataBase();
+
+            // Get the FragmentManager
+            FragmentManager fragmentManager = getSupportFragmentManager();
+
+            // Begin a fragment transaction
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+            // Replace the content frame with the new fragment
+            fragmentTransaction.replace(R.id.fragment_container, addWordOnDataBaseFragment);
+
+            // Commit the transaction
+            fragmentTransaction.commit();
+        }
     }
 }
